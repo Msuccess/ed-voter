2db100a51ed92aa347da0528541a40b18793d653
Moses Nkrumah <mosessccss@gmail.com>
2020-06-14 13:36:57 +0000

    initial commit

---
+++ src/entity/User.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import * as bcrypt from 'bcryptjs';
+import { Length, IsNotEmpty } from 'class-validator';
+
+@Entity()
+@Unique(['username'])
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @IsNotEmpty()
+  @Column()
+  email: string;
+
+  @Column()
+  @Length(4, 20)
+  username: string;
+
+  @Column()
+  @Length(4, 100)
+  password: string;
+
+  @Column()
+  @IsNotEmpty()
+  role: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
+}
